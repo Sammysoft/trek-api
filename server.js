cc793a1637a56854433ae1b0c8cf51d6e4d7b7eb
Samuel Dare-Owonibi <dareowonibisamuel@gmail.com>
2023-05-12 18:23:01 +0000

    making some api endpoints

--- server.js
+++ server.js
@@ -6,7 +6,20 @@ import Router from "./Routes/index.js";
 const server = express();
 server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
-server.use(cors());
+
+
+server.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type"
+    );
+    next();
+  });
+  server.use(cors());
+
 
 const Port  = process.env.PORT || 8089;
 
